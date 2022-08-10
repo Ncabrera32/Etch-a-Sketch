ac8aa80e69ed141b7c1219edd9de4853b915d29b
Ncabrera32 <nathanielcabrera88@yahoo.com>
2022-08-09 21:55:14 -0600

    set up body of html file with buttons and board. Created reset.css and created funciton to populate board

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Etch-a-Sketch
 
 1st commit set up main files
+
+2nd commit set up body of html file with buttons and board. Created reset.css and created funciton to populate board
--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-let color = "black";
+function populateBoard(size){
+let board = document.querySelector(".board");
+board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+for(let i = 0; i < 256; i++) {
+let square = document.createElement("div");
+    square.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend", square);
+}
+}
+
+populateBoard(16);
